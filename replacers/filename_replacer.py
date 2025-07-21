43625a3e25f13700cc71476efadc5e2690722b02
junweny <junweny@foxmail.com>
2025-07-21 11:00:34 +0800

    优化逻辑

--- replacers/filename_replacer.py
+++ replacers/filename_replacer.py
@@ -1,15 +1,5 @@
-import os
-import re
+# 移除所有更改文件名的逻辑，保留一个空实现或提示
 
 def replace_filename(file_path, replacements):
-    dir_name, base_name = os.path.split(file_path)
-    new_name = base_name
-    for old, new in replacements:
-        new_name = new_name.replace(old, new)
-    # 替换新文件名中的特殊字符（如！等）为下划线
-    new_name = re.sub(r'[！!@#$%^&*?<>|:"\'/]', '_', new_name)
-    if new_name != base_name:
-        new_path = os.path.join(dir_name, new_name)
-        os.rename(file_path, new_path)
-        return new_path
+    # 不再更改文件名，直接返回原路径
     return file_path 
